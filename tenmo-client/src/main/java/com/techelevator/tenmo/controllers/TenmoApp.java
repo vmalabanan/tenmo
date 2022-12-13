821f163f4bb24aa4ffe7b47f066640ca5e81f947
Gregor Dzierzon <gregor@fullstackdevs.net>
2022-12-12 23:29:14 -0700

    added starter code

---
+++ tenmo-client/src/main/java/com/techelevator/tenmo/controllers/TenmoApp.java
@@ -0,0 +1,144 @@
+package com.techelevator.tenmo.controllers;
+
+import com.techelevator.tenmo.models.AuthenticatedUser;
+import com.techelevator.tenmo.models.UserCredentials;
+import com.techelevator.tenmo.services.AuthenticationService;
+import com.techelevator.tenmo.views.UserOutput;
+
+public class TenmoApp
+{
+
+    private static final String API_BASE_URL = "http://localhost:8080/";
+
+    private final UserOutput userOutput = new UserOutput();
+    private final AuthenticationService authenticationService = new AuthenticationService(API_BASE_URL);
+
+    private AuthenticatedUser currentUser;
+
+    public void run()
+    {
+        userOutput.printGreeting();
+        loginMenu();
+        if (currentUser != null)
+        {
+            mainMenu();
+        }
+    }
+
+    private void loginMenu()
+    {
+        int menuSelection = -1;
+        while (menuSelection != 0 && currentUser == null)
+        {
+            userOutput.printLoginMenu();
+            menuSelection = userOutput.promptForMenuSelection("Please choose an option: ");
+            if (menuSelection == 1)
+            {
+                handleRegister();
+            }
+            else if (menuSelection == 2)
+            {
+                handleLogin();
+            }
+            else if (menuSelection != 0)
+            {
+                System.out.println("Invalid Selection");
+                userOutput.pause();
+            }
+        }
+    }
+
+    private void handleRegister()
+    {
+        System.out.println("Please register a new user account");
+        UserCredentials credentials = userOutput.promptForCredentials();
+        if (authenticationService.register(credentials))
+        {
+            System.out.println("Registration successful. You can now login.");
+        }
+        else
+        {
+            userOutput.printErrorMessage();
+        }
+    }
+
+    private void handleLogin()
+    {
+        UserCredentials credentials = userOutput.promptForCredentials();
+        currentUser = authenticationService.login(credentials);
+        if (currentUser == null)
+        {
+            userOutput.printErrorMessage();
+        }
+    }
+
+    private void mainMenu()
+    {
+        int menuSelection = -1;
+        while (menuSelection != 0)
+        {
+            userOutput.printMainMenu();
+            menuSelection = userOutput.promptForMenuSelection("Please choose an option: ");
+            if (menuSelection == 1)
+            {
+                viewCurrentBalance();
+            }
+            else if (menuSelection == 2)
+            {
+                viewTransferHistory();
+            }
+            else if (menuSelection == 3)
+            {
+                viewPendingRequests();
+            }
+            else if (menuSelection == 4)
+            {
+                sendBucks();
+            }
+            else if (menuSelection == 5)
+            {
+                requestBucks();
+            }
+            else if (menuSelection == 0)
+            {
+                continue;
+            }
+            else
+            {
+                System.out.println("Invalid Selection");
+            }
+            userOutput.pause();
+        }
+    }
+
+    private void viewCurrentBalance()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    private void viewTransferHistory()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    private void viewPendingRequests()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    private void sendBucks()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    private void requestBucks()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
