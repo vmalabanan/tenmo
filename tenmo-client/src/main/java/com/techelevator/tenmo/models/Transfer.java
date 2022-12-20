1e8293072ba60aa541ceb96104677674ad0d6470
Vanessa Malabanan <vanessamalabanan@yahoo.com>
2022-12-20 01:21:19 -0800

    work on displaying all transfers

--- tenmo-client/src/main/java/com/techelevator/tenmo/models/Transfer.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/models/Transfer.java
@@ -2,75 +2,66 @@ package com.techelevator.tenmo.models;
 
 import java.math.BigDecimal;
 
-// This class represents a trimmed-down version of a Transfer object,
-// composed only of userIdTo and amount
 public class Transfer {
-//    private int transferId;
-//    private int transferTypeId;
-//    private int transferStatusId;
-//    private int accountFrom;
-//    private int accountTo;
-
-    // Since client should not know the account number of the user they're sending money to,
-    // use userIdTo instead. We will not include the userId of the sender because this
-    // will be determined on the server side
+    private int transferId;
+    private int transferTypeId;
+    private int transferStatusId;
+    private int userIdFrom;
     private int userIdTo;
     private BigDecimal amount;
+    private String transferTypeDesc;
+    private String transferStatusDesc;
+    private String usernameFrom;
+    private String usernameTo;
+
+    // Since client should not know the account number of the user they're sending money to
+    // and server will determine the client's account number,
+    // Transfer won't keep track of account numbers
+    //    private int accountFrom;
+    //    private int accountTo;
 
     public Transfer() {
     }
 
-//    public Transfer(int transferId, int transferTypeId, int transferStatusId, int accountFrom, int accountTo, BigDecimal amount) {
-////        this.transferId = transferId;
-//        this.transferTypeId = transferTypeId;
-//        this.transferStatusId = transferStatusId;
-////        this.accountFrom = accountFrom;
-////        this.accountTo = accountTo;
-//        this.amount = amount;
-//    }
+    public int getTransferId() {
+        return transferId;
+    }
 
-//    public int getTransferId() {
-//        return transferId;
-//    }
-//
-//    public void setTransferId(int transferId) {
-//        this.transferId = transferId;
-//    }
+    public void setTransferId(int transferId) {
+        this.transferId = transferId;
+    }
 
+    public int getTransferTypeId() {
+        return transferTypeId;
+    }
 
+    public void setTransferTypeId(int transferTypeId) {
+        this.transferTypeId = transferTypeId;
+    }
 
+    public int getTransferStatusId() {
+        return transferStatusId;
+    }
 
-//    public int getTransferTypeId() {
-//        return transferTypeId;
-//    }
-//
-//    public void setTransferTypeId(int transferTypeId) {
-//        this.transferTypeId = transferTypeId;
-//    }
-//
-//    public int getTransferStatusId() {
-//        return transferStatusId;
-//    }
-//
-//    public void setTransferStatusId(int transferStatusId) {
-//        this.transferStatusId = transferStatusId;
-//    }
+    public void setTransferStatusId(int transferStatusId) {
+        this.transferStatusId = transferStatusId;
+    }
 
-//    public int getAccountFrom() {
-//        return accountFrom;
-//    }
-//
-//    public void setAccountFrom(int accountFrom) {
-//        this.accountFrom = accountFrom;
-//    }
-//
-//    public int getAccountTo() {
-//        return accountTo;
-//    }
-//
-//    public void setAccountTo(int accountTo) {
-//        this.accountTo = accountTo;
-//    }
+    public int getUserIdFrom() {
+        return userIdFrom;
+    }
+
+    public void setUserIdFrom(int userIdFrom) {
+        this.userIdFrom = userIdFrom;
+    }
+
+    public int getUserIdTo() {
+        return userIdTo;
+    }
+
+    public void setUserIdTo(int userIdTo) {
+        this.userIdTo = userIdTo;
+    }
 
     public BigDecimal getAmount() {
         return amount;
@@ -80,11 +71,35 @@ public class Transfer {
         this.amount = amount;
     }
 
-    public int getUserIdTo() {
-        return userIdTo;
+    public String getTransferTypeDesc() {
+        return transferTypeDesc;
     }
 
-    public void setUserIdTo(int userIdTo) {
-        this.userIdTo = userIdTo;
+    public void setTransferTypeDesc(String transferTypeDesc) {
+        this.transferTypeDesc = transferTypeDesc;
+    }
+
+    public String getTransferStatusDesc() {
+        return transferStatusDesc;
+    }
+
+    public void setTransferStatusDesc(String transferStatusDesc) {
+        this.transferStatusDesc = transferStatusDesc;
+    }
+
+    public String getUsernameFrom() {
+        return usernameFrom;
+    }
+
+    public void setUsernameFrom(String usernameFrom) {
+        this.usernameFrom = usernameFrom;
+    }
+
+    public String getUsernameTo() {
+        return usernameTo;
+    }
+
+    public void setUsernameTo(String usernameTo) {
+        this.usernameTo = usernameTo;
     }
 }
--- tenmo-server/src/main/java/com/techelevator/tenmo/model/Transfer.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/model/Transfer.java
@@ -1,5 +1,7 @@
 package com.techelevator.tenmo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.math.BigDecimal;
 
 public class Transfer {
@@ -7,10 +9,15 @@ public class Transfer {
     private int transferTypeId;
     private int transferStatusId;
     private int accountFrom;
+    @JsonIgnore // Client should not know accountTo
     private int accountTo;
     private int userIdFrom;
     private int userIdTo;
     private BigDecimal amount;
+    private String transferTypeDesc;
+    private String transferStatusDesc;
+    private String usernameFrom;
+    private String usernameTo;
 
     public int getTransferId() {
         return transferId;
@@ -76,9 +83,35 @@ public class Transfer {
         this.amount = amount;
     }
 
-//    @Override
-//    public String toString() {
-//
-//        return null;
-//    }
+    public String getTransferTypeDesc() {
+        return transferTypeDesc;
+    }
+
+    public void setTransferTypeDesc(String transferTypeDesc) {
+        this.transferTypeDesc = transferTypeDesc;
+    }
+
+    public String getTransferStatusDesc() {
+        return transferStatusDesc;
+    }
+
+    public void setTransferStatusDesc(String transferStatusDesc) {
+        this.transferStatusDesc = transferStatusDesc;
+    }
+
+    public String getUsernameFrom() {
+        return usernameFrom;
+    }
+
+    public void setUsernameFrom(String usernameFrom) {
+        this.usernameFrom = usernameFrom;
+    }
+
+    public String getUsernameTo() {
+        return usernameTo;
+    }
+
+    public void setUsernameTo(String usernameTo) {
+        this.usernameTo = usernameTo;
+    }
 }
