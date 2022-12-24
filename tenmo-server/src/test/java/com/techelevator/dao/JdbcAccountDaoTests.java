ff9f861a9460232429f6bb1ae67e7a8e5f24641f
Hong Le <hongle622@gmail.com>
2022-12-24 10:15:42 -0800

    Attempt 2 on tests

--- tenmo-server/src/test/java/com/techelevator/dao/JdbcAccountDaoTests.java
+++ tenmo-server/src/test/java/com/techelevator/dao/JdbcAccountDaoTests.java
@@ -2,18 +2,15 @@ package com.techelevator.dao;
 
 import com.techelevator.tenmo.dao.jdbc.JdbcAccountDao;
 import com.techelevator.tenmo.model.Account;
-import io.jsonwebtoken.lang.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
-
 import java.math.BigDecimal;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JdbcAccountDaoTests extends BaseDaoTests {
-    private static final Account ACCOUNT_1 = new Account(2001, 1001, BigDecimal.valueOf(550.00));
-    private static final Account ACCOUNT_2 = new Account(2002, 1002, BigDecimal.valueOf(600.00));
+    private static final Account ACCOUNT_1 = new Account(2001, 1001, BigDecimal.valueOf(1000));
+    private static final Account ACCOUNT_2 = new Account(2002, 1002, BigDecimal.valueOf(1000));
 
     private JdbcAccountDao sut;
 
@@ -24,27 +21,34 @@ public class JdbcAccountDaoTests extends BaseDaoTests {
     }
 
     @Test
-    public void getBalance_returns_correct_balance_for_userId() {
+    public void getBalance_returns_correct_balance_with_userId() {
         //arrange
-        BigDecimal expectedBalance = BigDecimal.valueOf(550.00);
+        BigDecimal expectedBalance = BigDecimal.valueOf(1000);
 
         //act
         BigDecimal actualBalance = sut.getBalance(ACCOUNT_1.getUserId());
 
         //assert
-        String message = "Because getBalance should return the correct balance for user id.";
+        String message = "Because getBalance should return the correct balance for the given user id.";
         assertEquals(expectedBalance, actualBalance, message);
+
+//        Assert.assertEquals(ACCOUNT_1.getBalance(), actualBalance);
 
     }
 
     @Test
-    public void getAccountId_returns_correct_accountId() {
+    public void getAccountId_returns_correct_accountId_with_userId() {
         //arrange
+        Integer expectedAccountId = 2002;
 
         //act
+        Integer actualAccountId = sut.getAccountId(ACCOUNT_2.getUserId());
 
         //assert
+        String message = "Because getAccountId should return the correct accountId using the user id";
+        assertEquals(expectedAccountId, actualAccountId, message);
     }
+
 
 
 
