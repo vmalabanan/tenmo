95b1eb9c6e3fe013cab34a43df9d906a71462445
Vanessa Malabanan <vanessamalabanan@yahoo.com>
2022-12-27 00:50:44 -0800

    edit tenmo logo

--- tenmo-client/src/main/java/com/techelevator/tenmo/views/TenmoLogo.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/views/TenmoLogo.java
@@ -42,7 +42,6 @@ public class TenmoLogo {
 //        System.out.println("/////.:~?YYY~^~///.::::::.//::^:::^^::.//::^:.::^::..::^::.///.::^^::////////");
 //        System.out.println("////'7JYYYYYJJJ:/^7?J??JJ7//7JJJ?JJJJJ!//7JJJ?JJJJJ??JJJJJ^//~7JJJJJJ?///////");
 //        System.out.println("//////~YYYY^///.!JJJ//!JJ?//?JJJ~^7JJJ7/:?JJJ~^7JJJ?^^?JJJ^/7JJJ7:^JJJJ//////");
-//        System.out.println("//////7YYY?////^JJJJ7?77~//~JJJ7//7JJJ~/~JJJ?//7JJJ~/:JJJ?./JJJ?:/:?JJJ//////");
 //        System.out.println("//////YYYY7..:/~JJJ?~:::^//7JJJ~//JJJ?:/7JJJ!/:JJJJ:/!JJJ7./JJJ?::!JJJ!//////");
 //        System.out.println("//////JYYYYJJ!//!?JJJJJJ?//JJJJ://JJJ7./JJJJ:/!JJJ7//?JJJ^//!JJJJJJJ?~///////");
 //        System.out.println("///////^~~~^//////^~~~^^///^^^^///^^^///^^^^///^^^///^^^^/////^~~~^//////////");
